8ae16043d94b35f43333f3196deccbfdd1d0fa7b
TradeSis Helio <helio@tradesis.com.br>
2024-03-01 15:11:08 -0300

    progress

---
+++ app/acordos/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/*VERSAO 2 23062021*/
+
+$progresscfg="progress.cfg";
+$dlc                = "/usr/dlc";
+//$proexe         = "$dlc/bin/_progres";
+$tmp            = "/u/bsweb/works";
+$pf                = "/admcom/bases/wsp2k.pf";
+#$pf                = "/admcom/scripts/hmlprd.pf";
+$propath        = "/u/bsweb/progr/app/,/admcom/progr/,";
+$proginicial        = "/u/bsweb/progr/app/acordos/chamaprogress.p";
+
+
+require ("../php/chamaprogress.php");
+?>
---
+++ app/seguro/config.php
@@ -0,0 +1,13 @@
+<?php
+$progresscfg="progress.cfg";
+$dlc                = "/usr/dlc";
+//$proexe         = "$dlc/bin/_progres";
+$tmp            = "/u/bsweb/works";
+$pf                = "/admcom/bases/wsp2k.pf";
+$propath        = "/u/bsweb/progr/app/,/admcom/progr/,";
+$proginicial        = "/u/bsweb/progr/php/chamaprogress.p";
+
+include ("../php/chamaprogress.php");
+
+
+?>
